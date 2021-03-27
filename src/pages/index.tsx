9b0b7938b6bf9862ee47e4b96ca1ca4680fa64f8
viniciusbe <vinicius.bernardesds@gmail.com>
2021-03-26 23:07:46 -0300

    feat: create _document, global styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1 >
+        hello <span>next</span>
+      </h1>
+    </>
+  )
+}
+
+//Recarrega a cada troca de página
